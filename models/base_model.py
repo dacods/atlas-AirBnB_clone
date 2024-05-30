ab965926d5eec2390873db52c8a023dd7faec1bb
LJThao <yengfong.thao@atlasschool.com>
2024-05-30 20:46:43 +0000

    added a square bracket to __str_()

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
 
     def __str__(self):
         """Function to print the string rep of the instance"""
-        return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}]"
 
     def save(self):
         """Function to update the updated_ at attribute with the current datetime"""
