3d0e63b0595563082d3cc79affc9dfd8fe86a75d
LJThao <yengfong.thao@atlasschool.com>
2024-05-30 20:41:11 +0000

    added quotation to fix syntax error

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
         self.updated_at = datetime.now()
         for key, value in kwargs.items():
             if key in ['created_at', 'updated_at']:
-                value = datetime.strptime(value, %Y-%m-%dT%H:%M:%S.%f)
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
             setattr(self, key, value)
 
     def __str__(self):
