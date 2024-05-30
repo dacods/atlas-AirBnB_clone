b99e6ccc2c5d787515567df77cc471d257e280ae
LJThao <yengfong.thao@atlasschool.com>
2024-05-30 21:23:49 +0000

    added class TestBaseModel in test file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Tests for BaseModel"""
+
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """Unittests for testing BaseModel"""
+    def test_base_save(self):
+        model = BaseModel()
+        model.save()
+        updated_at = model.updated_at
+        self.assertIsInstance(updated_at, datetime)
+
+if __name__ == '__main__':
+    unittest.main()
