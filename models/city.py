a5d9393655b7bfc353934848d1c7820fb6fa42d1
LJThao <yengfong.thao@atlasschool.com>
2024-05-31 22:43:47 +0000

    added code for state, city, amenity files

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for City Class"""
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class for City instance attributes"""
+    state_id = ""
+    name = ""
