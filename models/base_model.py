4c86bc7b90e5633aff4b0a14b66932b7f1ca9458
root <root@MrSlim3.>
2024-06-01 09:57:03 -0500

    Ran 5 test Status : OK

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,10 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Initializing BaseModel"""
+    """BaseModel class with common attributes/methods"""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -21,16 +23,15 @@ class BaseModel:
                 setattr(self, key, value)
 
     def __str__(self):
-        """Function to print the string rep of the instance"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}]"
+        """Return string representation of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Function to update the updated_ at attribute
-        with the current datetime"""
+        """Update the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Function to return the dictionary containing the keys and values"""
+        """Return dictionary representation of the BaseModel instance."""
         data_dict = self.__dict__.copy()
         data_dict['created_at'] = self.created_at.isoformat()
         data_dict['updated_at'] = self.updated_at.isoformat()
