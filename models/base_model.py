a2a036ada77e206f2f8c078de91812fac4d886d1
dacods <135403583+dacods@users.noreply.github.com>
2024-06-01 15:14:41 -0500

    Added instances of storage to save and __init__ in base_model

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        models.storage.new(self)
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -30,6 +31,7 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return dictionary representation of the BaseModel instance."""
