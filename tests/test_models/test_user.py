a9d8730f3e32aebb478156bb78f617ffe10b6e83
dacods <135403583+dacods@users.noreply.github.com>
2024-06-04 19:26:50 -0500

    Made changes in test_user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -23,5 +23,16 @@ class TestUserModel(unittest.TestCase):
         self.assertEqual(self.user.first_name, "")
         self.assertEqual(self.user.last_name, "")
 
+    def test_values(self):
+        """Testing values"""
+        self.user.email = "airbnb@mail.com"
+        self.user.password = "password"
+        self.user.first_name = "Betty"
+        self.user.last_name = "Bar"
+        self.assertEqual(self.user.email, "airbnb@mail.com")
+        self.assertEqual(self.user.password, "password")
+        self.assertEqual(self.user.first_name, "Betty")
+        self.assertEqual(self.user.last_name, "Bar")
+
 if __name__ == "__main__":
     unittest.main()
